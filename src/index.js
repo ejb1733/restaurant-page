03629e939d111218043a47a3bf5e9ec96e9bb5c5
ejb1733 <ethanjbillings@gmail.com>
2022-06-21 17:53:50 -0700

    Add buttons to display / remove current page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import displayHomePage from './homepage.js';
 
+const hp = displayHomePage();
+
 const body = document.querySelector('body');
+
+const btnDiv = document.createElement('div');
 const content = document.createElement('div');
+const homeBtn = document.createElement('button');
+const menuBtn = document.createElement('button');
+const contactBtn = document.createElement('button');
+
+btnDiv.setAttribute('id', 'btndiv');
 content.setAttribute('id', 'content');
+homeBtn.setAttribute('id', 'homebtn');
+menuBtn.setAttribute('id', 'menubtn');
+contactBtn.setAttribute('id', 'contactbtn');
+
+btnDiv.appendChild(homeBtn);
+btnDiv.appendChild(menuBtn);
+btnDiv.appendChild(contactBtn);
+
+content.appendChild(hp);
+
+body.appendChild(btnDiv);
 body.appendChild(content);
 
-content.appendChild(displayHomePage());
+homeBtn.addEventListener('click', () => {
+  hp.remove();
+});
+
+menuBtn.addEventListener('click', () => {
+  console.log('append');
+  content.appendChild(hp);
+});
 
 console.log('hiii');
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
-  entry: {
-    index: './src/index.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Restaurant',
-    }),
-  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
